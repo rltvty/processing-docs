84dc77010781b9974bff331e8a4d469ad4704dae
Eric Pinzur <E@Erics-Air.fritz.box>
2017-06-13 08:39:30 +0200

    add generated files

---
+++ index.php
@@ -0,0 +1,284 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>
+		<title>Processing.org</title>
+		
+		<link rel="icon" href="/favicon.ico" type="image/x-icon" />
+		
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<meta name="Author" content="Processing Foundation" />
+		<meta name="Publisher" content="Processing Foundation" />
+		<meta name="Keywords" content="Processing, Sketchbook, Programming, Coding, Code, Art, Design" />
+		<meta name="Description" content="Processing is a flexible software sketchbook and a language for learning how to code within the context of the visual arts. Since 2001, Processing has promoted software literacy within the visual arts and visual literacy within technology." />
+		<meta name="Copyright" content="All contents copyright the Processing Foundation, Ben Fry, Casey Reas, and the MIT Media Laboratory" />
+	    
+	    <script src="/javascript/modernizr-2.6.2.touch.js" type="text/javascript"></script>
+		<link href="/css/style.css" rel="stylesheet" type="text/css" />
+	</head>
+	<body id="Cover" onload="" >
+		
+		<!-- ==================================== PAGE ============================ --> 
+		<div id="container">
+	
+			<!-- ==================================== HEADER ============================ --> 
+			<div id="ribbon">
+				<ul class="left">
+					<li class="highlight"><a href="http://processing.org/">Processing</a></li>
+					<li><a href="http://p5js.org/">p5.js</a></li>
+					<li><a href="http://py.processing.org/">Processing.py</a></li>
+					<li><a href="http://android.processing.org/">Processing for Android</a></li>
+				</ul>
+				<ul class="right">
+					<li><a href="https://processingfoundation.org/">Processing Foundation</a></li>
+				</ul>
+				<div class="clear"></div>
+			</div>
+			<div id="header">
+				<div class="processing-logo" alt="Processing cover"></div>
+				<form name="search" action="//www.google.com/search" method="get">
+				<!--<label>Search processing.org</label>-->
+				       <p><input type="hidden" name="as_sitesearch" value="processing.org" />
+				       <input type="text" name="as_q" value="" size="20" class="text" /> 
+						<input type="submit" value=" " /></p>
+				</form>
+			</div> 
+			<a id="TOP" name="TOP"></a>
+			
+			<!-- ==================================== NAVIGATION ============================ -->
+						<div id="navigation">
+				<div class="navBar" id="mainnav_noSub">
+					<a href="/" class="active">Cover</a><br><br>
+					<a href="/download/">Download</a><br>
+					<a href="/download/support.html">Donate</a><br><br>
+					<a href="/exhibition/">Exhibition</a><br><br>
+					<a href="/reference/">Reference</a><br>
+					<a href="/reference/libraries/">Libraries</a><br>
+					<a href="/reference/tools/">Tools</a><br>
+					<a href="/reference/environment/">Environment</a><br><br>
+					<a href="/tutorials/">Tutorials</a><br>
+					<a href="/examples/">Examples</a><br>
+					<a href="/books/">Books</a><br>
+					<a href="/handbook/">Handbook</a><br><br />
+					<a href="/overview/">Overview</a><br> 
+					<a href="/people/">People</a><br><br>
+					<a href="/shop/">Shop</a><br><br>
+					<a href="http://forum.processing.org"class="outward"><span>&raquo;</span>Forum</a><br> 
+					<a href="https://github.com/processing"class="outward"><span>&raquo;</span>GitHub</a><br> 
+					<a href="https://github.com/processing/processing/issues?state=open"class="outward"><span>&raquo;</span>Issues</a><br> 
+					<a href="https://github.com/processing/processing/wiki/"class="outward"><span>&raquo;</span>Wiki</a><br> 
+					<a href="https://github.com/processing/processing/wiki/FAQ"class="outward"><span>&raquo;</span>FAQ</a><br> 
+					<a href="https://twitter.com/processingOrg"class="outward"><span>&raquo;</span>Twitter</a><br> 
+					<a href="https://www.facebook.com/page.processing"class="outward"><span>&raquo;</span>Facebook</a><br> 
+					<a href="https://medium.com/@ProcessingOrg"class="outward"><span>&raquo;</span>Medium</a><br> 
+				</div>
+			</div>
+
+
+			<!-- ==================================== CONTENT ============================ -->
+			<div class="content">
+			
+			 <div class="leftcol">
+
+    <!-- Note: Change the Video URL and the LINK as well. -->
+    <p class="video"><iframe src="https://player.vimeo.com/video/140600280" width="392" height="220" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+        <span class="video-caption">Welcome to Processing 3! Dan explains the new features and changes; the links Dan mentions are on the <a href="https://vimeo.com/140600280">Vimeo page</a>.</span></p>     
+
+    <a href="./download/" class="large-link">Download Processing</a><br />
+    <a href="./tutorials/" class="large-link">Browse Tutorials</a><br />
+    <a href="./reference/" class="large-link">Visit the Reference</a><br />
+    
+    <div class="intro">
+
+        <p>
+            Processing is a flexible software sketchbook and a language for learning how to code within the context of the visual arts. Since 2001, Processing has promoted software literacy within the visual arts and visual literacy within technology. There are tens of thousands of students, artists, designers, researchers, and hobbyists who use Processing for learning and prototyping.
+        
+            <ul>
+            	<li>&raquo; Free to download and open source</li>
+            	<li>&raquo; Interactive programs with 2D, 3D or PDF output</li>
+            	<li>&raquo; OpenGL integration for accelerated 2D and 3D</li>
+            	<li>&raquo; For GNU/Linux, Mac OS X, Windows, Android, and ARM</li>
+            	<li>&raquo; Over 100 libraries extend the core software</li>
+            	<li>&raquo; Well <a href="./reference/">documented</a>, with many <a href="./books/">books</a> available</li>
+            </ul>
+            <br />
+        </p>
+    </div>
+
+    <div class="intro">
+
+        <p>
+            <a href="./books/" class="large-link">Books</a><br />
+            <br />
+            The 2nd edition of <a href="./books/#reasfry2">Getting Started with Processing</a> is here and it's updated for Processing 3. It's now in full color and there's a new chapter on working with data. The 2nd edition of the <a href="./handbook/">Processing Handbook</a> is here too. Every chapter has been revised, and new chapters introduce more ways to work with data and geometry. New “synthesis” chapters offer discussion and worked examples of such topics as sketching with code, modularity, and algorithms.<br />
+            <br />
+            <a href="./books/"><img src="/img/learning/books/processing-second-edition.png" width="366" height="242" alt="New Book!" /></a>
+        </p>
+
+        <!--
+        <p>
+            <a href="http://hourofcode.com/pr" class="large-link">Hello Processing Videos</a><br />
+            <br />
+            This first look at Processing for total beginners is an introduction to programming in the context of the visual arts. Short video lessons introduce coding exercises that lead to designing an interactive drawing program. This experimental tutorial was created for Code.org's Hour of Code project for <a href="http://csedweek.org/">Computer Science Education Week</a>.<br />
+            <br />
+            <a href="http://hourofcode.com/pr"><img src="/img/drawing-with-code.jpg" width="392" height="294" /></a>
+        </p>
+        -->
+
+        <!--
+        <p class="video"><span class="video-caption">Welcome to Processing 3. In this video, Dan gives an overview of what's new.</span>
+    <iframe src="http://player.vimeo.com/video/60735314?title=0&amp;byline=0&amp;portrait=0&amp;color=ffffff" width="392" height="221" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
+    <span class="video-caption">from <a href="#">Ultra_Lab</a></span></p> 
+    -->   
+
+		
+    </div>
+
+    <div class="twitter">
+    	<h5>&raquo; <a href="https://twitter.com/processingOrg">Twitter</a></h5>
+    	<ul class="latest-tweets"></ul>
+    </div>
+
+    <div class="commits">
+    	<h5>&raquo; <a href="https://github.com/processing">Github</a></h5>
+    	<ul class="latest-commits"></ul>
+    </div>
+
+</div>
+
+<div class="rightcol">
+
+    <!--
+    <div class="exhibition-small">
+        <h3><a href="/shop/" class="large-link">New T-shirts!</a></h3>
+        <a href="/shop/"><img src="/shop/imgs/tshirts2013-navy-sm.jpg" width="223" height="144" /></a>
+        <p>To commemorate the release of Processing 2, these new tees are now available. Every shirt is handprinted on American Apparel 100% cotton shirts. 100% of the profits from these shirts fund the <a href="/foundation/">Processing Foundation</a>.</p>
+    </div>
+    -->
+
+    <!--
+	<div class="exhibition-small">
+        <h3><a href="/download/" class="large-link">Fundraiser 2013</a></h3>
+        <p>To celebrate the launch of the new Processing <a href="https://github.com/atduskgreg/opencv-processing">OpenCV library</a>, <a href="http://www.oreilly.com/">O'Reilly Media</a> is matching donations to the Processing Foundation up to $3000 for the remainder of 2013. Please donate by selecting the <a href="/download/">Download link</a>.</p>
+    </div>
+    -->
+
+    <div class="exhibition-small">
+        <h3><a href="/download/support.html" class="large-link">Donate</a></h3>
+        Please join us as a member of the Processing Foundation. <a href="https://medium.com/@ProcessingOrg/we-need-your-help-18936655ca4f">We need your help!</a>
+    </div>
+
+    <div class="exhibition-small">
+        <h3><a href="/exhibition/" class="large-link">Exhibition</a></h3>
+		<div class="curated-item">
+	<a href="http://www.rlfbckr.org/work/random-access-memory/"><img src="/exhibition/images/random.jpg" width="223" height="72" alt="preview image" title="Random Access Memory by Ralf Baecker" /></a>
+	<p><a href="http://www.rlfbckr.org/work/random-access-memory/">Random Access Memory</a><br />
+	by Ralf Baecker</p>
+</div>
+
+<div class="curated-item">
+	<a href="http://www.creativeapplications.net/processing/aadrl-behavioural-complexity/"><img src="/exhibition/images/aadrl.jpg" width="223" height="72" alt="preview image" title="Behavioural Complexity by AADRL" /></a>
+	<p><a href="http://www.creativeapplications.net/processing/aadrl-behavioural-complexity/">Behavioural Complexity</a><br />
+	by AADRL</p>
+</div>
+
+<div class="curated-item">
+	<a href="http://www.terrapattern.com"><img src="/exhibition/images/terrapattern.jpg" width="223" height="72" alt="preview image" title="Terrapattern by Golan Levin, David Newbury, Kyle McDonald, Irene Alvarado, Aman Tiwari, Manzil Zaheer and The Frank-Ratchye STUDIO for Creative Inquiry" /></a>
+	<p><a href="http://www.terrapattern.com">Terrapattern</a><br />
+	by Golan Levin, David Newbury, Kyle McDonald, Irene Alvarado, Aman Tiwari, Manzil Zaheer and The Frank-Ratchye STUDIO for Creative Inquiry</p>
+</div>
+
+<div class="curated-item">
+	<a href="http://reinoudvanlaar.nl/project/leavespattern/"><img src="/exhibition/images/FluidLeaves.jpg" width="223" height="72" alt="preview image" title="Fluid Leaves by Reinoud van Laar" /></a>
+	<p><a href="http://reinoudvanlaar.nl/project/leavespattern/">Fluid Leaves</a><br />
+	by Reinoud van Laar</p>
+</div>
+
+
+    </div>
+
+    <!--
+    <div class="exhibition-small">
+        <h3><a href="/shop/" class="large-link">T-shirts!</a></h3>
+        <a href="/shop/"><img src="/shop/imgs/tshirts2013-navy-sm.jpg" width="223" height="144" /></a>
+        <p>100% of the profits from these shirts fund the <a href="/foundation/">Processing Foundation</a>.</p>
+    </div>-->
+
+    <p class="italic">To see more of what people are doing with Processing, check out these sites:</p>
+
+	<p>&raquo; <a href="http://www.creativeapplications.net/category/processing/">CreativeApplications.Net</a><br />
+       &raquo; <a href="http://www.openprocessing.org/">OpenProcessing</a><br />
+       &raquo; <a href="http://fyprocessing.tumblr.com/">For Your Processing</a><br />
+       &raquo; <a href="https://www.reddit.com/r/processing/">Processing Subreddit</a><br />
+	   &raquo; <a href="https://vimeo.com/groups/processing">Vimeo</a><br />
+       <!--&raquo; <a href="http://www.flickr.com/groups/processing/">Flickr</a><br />-->
+       &raquo; <a href="http://sketchpad.cc/">Studio Sketchpad</a><br />
+	</p>
+				
+    <p class="cover-contrib italic">
+    	To contribute to the development, please visit 
+    	<a href="https://github.com/processing">Processing on GitHub</a> to read 
+        instructions for downloading the code, 
+        <a href="https://github.com/processing/processing/wiki/Build-Instructions">building from the source</a>, 
+        <a href="http://wiki.processing.org/w/Report_Bugs">reporting and tracking bugs</a>, and 
+        creating <a href="https://github.com/processing/processing/wiki">libraries and tools</a>.</p>
+
+	</p>
+
+	<p class="partners">
+		<h5>Partners</h5>
+		&raquo; <a href="http://fathom.info/">Fathom</a><br />
+        &raquo; <a href="http://itp.nyu.edu/itp/">NYU ITP</a><br />
+        &raquo; <a href="http://dma.ucla.edu/">UCLA Design Media Arts</a>
+	</p>
+
+	<!--<div class="mailing-list">
+		<h5>Mailing List</h5>
+		<form action="/subscribe/" method="post">
+	        <input name="email" value="Email address" type="email" width="35"
+	        	   onfocus="if(this.value == 'Email address') { this.value = ''; }" 
+				   onblur="if(this.value == '') { this.value = 'Email address'; }">
+	        <input name="submit" value="Join" type="submit">
+	    </form><br>
+	    If you are interested in receiving updates about Processing, submit your email through this form. Your email will only be used to send infrequent updates about Processing. It will not be sold or shared.
+	</div>-->
+
+	<p class="contact">
+		<h5 style="margin:0;">Contact</h5>
+		<a href="mailto:foundation@processing.org">foundation@processing.org</a>
+	</p>
+
+</div>
+
+			
+			</div>
+
+			<!-- ==================================== FOOTER ============================ --> 
+  			<div id="footer">
+    			<div id="copyright">Processing was initiated by <a href="http://benfry.com/">Ben Fry</a> and <a href="http://reas.com">Casey Reas</a>. It is developed by a <a href="/people/">small team of volunteers</a>.</div> 
+    			<div id="colophon">
+
+                    <a href="/copyright.html">&copy; Info</a>
+
+                </div>
+  			</div>
+  			
+		</div>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script>window.jQuery || document.write('<script src="/javascript/jquery-1.9.1.min.js"><\/script>')</script>
+		<script src="/javascript/site.js" type="text/javascript"></script>
+		<script type="text/javascript">
+
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-40016511-1']);
+		  _gaq.push(['_setDomainName', 'processing.org']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+
+		</script>
+	</body>
+</html>
